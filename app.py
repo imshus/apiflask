eb54263bca198675e95027288ba1baa380c1e0f3
imshus <67273421+imshus@users.noreply.github.com>
2024-10-19 19:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify,ssl_support
 from pymongo import MongoClient
 from flask_cors import CORS
+import ssl
 
 app = Flask(__name__)
 CORS(app)
