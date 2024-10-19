f041b4018455a958130297ef51ab58844a21b74f
imshus <67273421+imshus@users.noreply.github.com>
2024-10-19 18:50:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ def get_all_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 4000))  # Default to 5000 if PORT isn't set
+    app.run(host='0.0.0.0', port=port)
 
