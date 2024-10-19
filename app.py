9e577af046b54f32a49732a9d8b5771e64c272ad
imshus <67273421+imshus@users.noreply.github.com>
2024-10-19 20:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Set up MongoDB connection
 client = MongoClient("mongodb+srv://imshu1:imshu1@cluster0.cagck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
-db = client.dashboard_db  # Database
-collection = db.insights  # Collection
+db = client.dashboard_db 
+collection = db.insights 
 
-# Route to load JSON data into MongoDB
 @app.route('/load-data', methods=['GET'])
 def load_data():
     try:
-        # Load JSON file
+     
         with open('data/jsondata.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
 
-        # Insert data into MongoDB collection
+     
         collection.insert_many(data)
 
         return jsonify({'message': 'Data loaded successfully!'}), 200
@@ -27,11 +25,11 @@ def load_data():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# Route to verify if data is loaded
+
 @app.route('/data', methods=['GET'])
 def get_data():
     try:
-        data = list(collection.find({}, {"_id": 0}))  # Return data without the MongoDB _id field
+        data = list(collection.find({}, {"_id": 0}))  
         return jsonify(data), 200
 
     except Exception as e:
