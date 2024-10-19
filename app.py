3e3e2f6a7a0021fcc73ee5072522fe8255a088c7
imshus <67273421+imshus@users.noreply.github.com>
2024-10-19 19:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-import os
-from flask import Flask, json, jsonify
+from flask import Flask, jsonify
 from pymongo import MongoClient
+import json
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app)
 
 # Set up MongoDB connection
-client = MongoClient("mongodb+srv://imshu1:imshu1@cluster0.cagck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://imshu1:imshu1@cluster0.cagck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",ssl=True)  # Replace with your MongoDB connection string
 db = client.dashboard_db  # Database
 collection = db.insights  # Collection
 
-@app.route('/load-data',methods=['GET'])
+# Route to load JSON data into MongoDB
+@app.route('/load-data', methods=['GET'])
 def load_data():
     try:
         # Load JSON file
-        with open('data/jsondata.json', 'r') as file:
+        with open('data/jsondata.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
 
         # Insert data into MongoDB collection
@@ -27,21 +27,18 @@ def load_data():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# API route to get all data
-@app.route('/api/data', methods=['GET'])
-def get_all_data():
+# Route to verify if data is loaded
+@app.route('/data', methods=['GET'])
+def get_data():
     try:
-        # Fetch all data from MongoDB
-        data = list(collection.find({}, {"_id": 0}))  # Avoid returning MongoDB _id field
+        data = list(collection.find({}, {"_id": 0}))  # Return data without the MongoDB _id field
         return jsonify(data), 200
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 4000))  # Default to 5000 if PORT isn't set
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True,host='0.0.0.0',port=4000)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
